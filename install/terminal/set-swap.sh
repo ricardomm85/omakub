25595e45441779f60caab4ad42760efdda27c85a
ricardomm <ricardo@scoutbasketball.com>
2024-09-12 11:52:32 +0200

    added set swap

---
+++ install/terminal/set-swap.sh
@@ -0,0 +1,14 @@
+
+gum style --foreground 3 "Configuring swap..."
+
+sudo fallocate -l 16G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+sudo sysctl vm.swappiness=10
+echo 'vm.swappiness=10' | sudo tee -a /etc/sysctl.conf
+sudo sysctl vm.vfs_cache_pressure=50
+echo 'vm.vfs_cache_pressure=50' | sudo tee -a /etc/sysctl.conf
+
+gum style --foreground 3 "Swap configuration completed."
