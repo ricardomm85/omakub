c3214580d65745b4861404508bfcfc81f49edb48
Ricardo <ricardo@scoutbasktball.com>
2025-05-16 09:17:05 +0200

    Show the battery percentage in the Gnome top bar

--- install/desktop/set-gnome-settings-custom.sh
+++ install/desktop/set-gnome-settings-custom.sh
@@ -1,3 +1,6 @@
 # Reveal weekday & seconds in the Gnome clock
 gsettings set org.gnome.desktop.interface clock-show-seconds true
 gsettings set org.gnome.desktop.interface clock-show-weekday true
+
+# Show the battery percentage in the Gnome top bar
+gsettings set org.gnome.desktop.interface show-battery-percentage true
